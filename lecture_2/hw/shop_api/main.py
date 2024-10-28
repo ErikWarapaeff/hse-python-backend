dd51d8de1b2e7bf1d855caed6f185d296dc6f8d2
Erik Varapaev <109546561+ErikWarapaeff@users.noreply.github.com>
2024-10-28 21:09:41 +0300

    Update main.py

--- lecture_2/hw/shop_api/main.py
+++ lecture_2/hw/shop_api/main.py
@@ -1,3 +1,8 @@
 from fastapi import FastAPI
 
+from lecture_2.hw.shop_api.app.routers.cart import router as cart_router
+from lecture_2.hw.shop_api.app.routers.item import router as item_router
+
 app = FastAPI(title="Shop API")
+app.include_router(item_router)
+app.include_router(cart_router)
